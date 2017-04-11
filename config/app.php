b0ecc899464af80ba209a8952e8f49dc27990d66
Marcin Matyska <mmatyska@n-educatio.pl>
2017-04-11 12:29:32 +0200

    added barryvdh/laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -178,6 +179,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         SammyK\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class,
+        'Barryvdh\Debugbar\ServiceProvider',
+
 
     ],
 
@@ -228,6 +231,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
+
 
     ],
 
